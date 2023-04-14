55d979573b3f075303731cf36d53f819bc5d834e
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2023-04-14 11:52:41 +0200

    next iteration 4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model.js");
+const db = require("../db/index.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.get("/movies", (req, res) => {
+    Movie.find()
+      .then((moviesFromDB) => {
+        res.render("movies", { movies: moviesFromDB });
+      })
+      .catch((err) => console.log(err));
+  });
 
 module.exports = router;
